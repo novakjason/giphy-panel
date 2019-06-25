fd2b120bb2dab36977fb4902aa82609089c84cc2
Red Tail <redtail814@gmail.com>
2019-06-24 22:40:31 -0400

    Added ajax function with for loop to call GIPHY API and populate gifs and ratings to page.  Created dynamic button array and added title attribute to each button.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,61 @@
 // 1. Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
 //    * We chose animals for our theme, but you can make a list to your own liking.
+$(function(){
+    createButton(buttonArray, 'giphyButton btn btn-custom', "#giphyButtons");
+})
 
-var topics = ["Dragon Ball Z", "Cowboy Bebop", "Ninja Scroll"];
+var buttonArray = ["Dragon Ball Z", "Cowboy Bebop", "Ninja Scroll"];
+
+function createButton(buttonArray, classes, gridLocation) {
+    $(gridLocation).empty();
+
+    for (var i = 0; i < buttonArray.length; i++) {
+
+        var animeButton = $("<button>");
+
+        animeButton.addClass(classes);
+        animeButton.attr("title", buttonArray[i]);
+        animeButton.text(buttonArray[i]);
+
+        $(gridLocation).append(animeButton);
+
+    }
+}
+
+$(document).on("click", ".giphyButton", function() {
+    var title = $(this).attr("title");
+    console.log(title);
+})
+
+
+var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + title + '&api_key=s471qPkGHmKx8PGao66Ogh34CZ1dQHpQ&limit=10';
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+
+    var results = response.data;
+
+    for (var j = 0; j < results.length; j++) {
+
+        var giphyDiv = $("<div>");
+        var animeImage = $("<img>");
+        var ratingDiv = $("<div>").text("Rating: " + results[j].rating);
+
+        animeImage.attr("src", results[j].images.fixed_height_still.url);
+
+        giphyDiv.addClass("col-md mb-1");
+        ratingDiv.addClass("col-md mb-3");
+
+        giphyDiv.append(animeImage);
+        giphyDiv.append(ratingDiv);
+
+        $("#gifs").prepend(giphyDiv);
+
+    }
+});
 
 // 2. Your app should take the topics in this array and create buttons in your HTML.
 //    * Try using a loop that appends a button for each string in the array.
